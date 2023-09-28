f8d3f9363106aee2f525f2b71b707ffe8b427791
KrutovGlib <krutov.hlib@chnu.edu.ua>
2023-09-28 21:06:22 +0300

    First Task

---
+++ src/Fraction.java
@@ -0,0 +1,73 @@
+public class Fraction {
+    private long numerator;   // Чисельник
+    private int denominator;  // Знаменник
+
+    // Конструктор для ініціалізації об'єкта Fraction
+    public Fraction(long numerator, int denominator) {
+        if (denominator == 0) {
+            throw new IllegalArgumentException("Знаменник не може бути рівним нулю.");
+        }
+
+        this.numerator = numerator;
+        this.denominator = denominator;
+        simplify(); // Спрощуємо дріб під час ініціалізації
+    }
+
+    // Метод для знаходження НСД (найменшого спільного дільника)
+    private long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+
+    // Метод для спрощення дробу до найменшого вигляду
+    private void simplify() {
+        long gcd = gcd(Math.abs(numerator), Math.abs(denominator));
+        numerator /= gcd;
+        denominator /= gcd;
+    }
+
+    // Метод для додавання дробів
+    public Fraction add(Fraction other) {
+        long newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    // Метод для віднімання дробів
+    // Метод для віднімання дробів
+    public Fraction subtract(Fraction other) {
+        long newNumerator = this.numerator * other.denominator - other.numerator * this.denominator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+
+    // Метод для множення дробів
+    public Fraction multiply(Fraction other) {
+        long newNumerator = this.numerator * other.numerator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    // Метод для порівняння дробів
+    public int compareTo(Fraction other) {
+        //різниця між чисельниками яка помножена на знаменик
+        long diff = this.numerator * other.denominator - other.numerator * this.denominator;
+        if (diff < 0) {
+            return -1;
+        } else if (diff > 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    // Перевизначення методу toString для зручного виводу дробу
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,66 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Java Lab 2 ");
-	// write your code here
+        System.out.println("Menu"+"\n"+"First task --> 1"+"\n"+"Second task --> 2"+"\n"+"Third task --> 3");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Your choose: ");
+        int switcher = scan.nextInt();
+        switch (switcher){
+            case 1: Firstask();
+        }
 
+    }
+
+    public static void Firstask(){
+        Scanner scan = new Scanner(System.in);
+        boolean look = true;
+
+
+        while (look == true) {
+            System.out.print("Enter the expression: ");
+
+            long num = scan.nextLong();
+            int den = scan.nextInt();
+            String symbol = scan.next();
+            long nextnum = scan.nextLong();
+            int nextden = scan.nextInt();
+
+            Fraction first = new Fraction(num,den);
+            Fraction second = new Fraction(nextnum,nextden);
+            switch (symbol) {
+                case "+": {
+                    Fraction sum = first.add(second);
+                    System.out.println("Sum of fractions: " + sum);
+                    break;
+                }
+                case "-": {
+                    Fraction substraction = first.subtract(second);
+                    System.out.println("Substraction of fractions: " + substraction);
+                    break;
+                }
+                case "*": {
+                    Fraction myltiply = first.multiply(second);
+                    System.out.println("Product of fractions: " + myltiply);
+                    break;
+                }
+                case "?": {
+                    int compareto = first.compareTo(second);
+                    if (compareto < 0) {
+                        System.out.println(first + " less than " + second);
+                    } else if (compareto > 0) {
+                        System.out.println(first + " more than " + second);
+                    } else {
+                        System.out.println(first + " equals " + second);
+                    }
+                }
+                case "0":
+                    look = false;
+                    break;
+                default:
+                    System.out.println("Invalide operator");
+            }
+        }
     }
 }
